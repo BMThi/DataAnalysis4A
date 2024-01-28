9513aa60a070cc944a2d723de00240e574a78703
Juliette Chevallier <juliette.chevallier@insa-toulouse.fr>
2024-01-28 12:00:07 +0100

    update readme

--- Velib/solutions/R/hilltop_stations_map.r
+++ Velib/solutions/R/hilltop_stations_map.r
@@ -1,5 +1,5 @@
 options(repr.plot.width = 10, repr.plot.height = 10)
 
-qmplot(data=df, longitude, latitude, color=hill) +
+qmplot(data=coord, longitude, latitude, color=hill) +
     scale_color_hue(direction = -1) +
     labs(title = 'Hilltop stations')
