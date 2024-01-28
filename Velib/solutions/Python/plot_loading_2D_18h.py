3acc3f8e01d8ff4d231f7c7a2b1ed90a38774d46
Juliette Chevallier <juliette.chevallier@insa-toulouse.fr>
2024-01-28 11:49:54 +0100

    typos correction

--- Velib/solutions/Python/plot_loading_2D_18h.py
+++ Velib/solutions/Python/plot_loading_2D_18h.py
@@ -1,7 +1,7 @@
 ## Simple 2D representation
 # Loading at 6pm, depending on the day of the week
 
-h     = 18
+h = 18
 hours = np.arange(h, 168, 24)
 
 load_per_hour = loading_data[:, hours]
@@ -12,19 +12,20 @@ days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sun
 
 s, m = 10, 3
 k = 1 + len(days)//m
-fig, axs = plt.subplots(k, m, figsize = (s*(m+.5), s*k))
+
+fig = plt.figure(figsize=(s+1, s))
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=.3, wspace=.25)
 
 for (i,d) in enumerate(days):
-    loc = i//m, i-m*(i//m)
-    im  = axs[loc].scatter(coord.latitude, coord.longitude, c = load_per_hour[:,i], cmap = cm.plasma_r)
-    axs[loc].set_title('Stations loading - ' + d + ' {} h'.format(h), fontsize = 25)
-    plt.colorbar(im, ax=axs[loc])
-        
-for ax in axs.flat:
-    ax.set_xlabel('Latitude', fontsize = 20)
-    ax.set_ylabel('Longitude', fontsize = 20)
-    ax.tick_params(axis='x', labelsize=15)
-    ax.tick_params(axis='y', labelsize=15)
+    ax = fig.add_subplot(k, m, i+1)
+    im = ax.scatter(coord.latitude, coord.longitude, c = load_per_hour[:,i], cmap = cm.plasma_r)
+    plt.colorbar(im)
+    
+    ax.set_title('Stations loading - ' + d + ' {} h'.format(h))
+    ax.set_xlabel('Latitude')
+    ax.set_ylabel('Longitude')
+    ax.tick_params(axis='x')
+    ax.tick_params(axis='y')
 
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
