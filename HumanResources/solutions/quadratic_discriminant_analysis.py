c92b98aa049f84c2bed1a8d412c91bbe62ebb5ed
Juliette Chevallier <juliette.chevallier@insa-toulouse.fr>
2024-02-07 09:27:05 +0100

    maj TP HR

--- HumanResources/solutions/quadratic_discriminant_analysis.py
+++ HumanResources/solutions/quadratic_discriminant_analysis.py
@@ -6,7 +6,7 @@ clf.fit(train_x, train_y)
 pred_y = clf.predict(test_x)
 
 accuracy = accuracy_score(test_y, pred_y, normalize=True, sample_weight=None)
-print('Accuray:',round(accuracy,3))
+print('Accuray:',round(100*accuracy,2), '%')
 print(' ')
 
 # --- #
